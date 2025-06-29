f631f1ecb7ab4e14f6f7bbb52fd298182a7de16c
Sravan Kumar <sravankumars@gmail.com>
2025-06-29 02:54:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/companies/page.tsx
+++ src/app/(app)/companies/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Header } from "@/components/header";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { companies } from "@/lib/data";
