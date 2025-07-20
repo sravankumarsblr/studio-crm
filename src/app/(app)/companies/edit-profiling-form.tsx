c44d761159ce7cdb4653852bb990b7f3bee3d8c2
Sravan Kumar <sravankumars@gmail.com>
2025-07-20 05:27:34 +0000

    since there are more feilds, make UI to 3 column layout and increase the

--- src/app/(app)/companies/edit-profiling-form.tsx
+++ src/app/(app)/companies/edit-profiling-form.tsx
@@ -115,7 +115,7 @@ export function EditProfilingForm({
             <TabsTrigger value="behavioral">Behavioral & Relational</TabsTrigger>
           </TabsList>
           <TabsContent value="firmographic" className="pt-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
               <FormField
                 control={form.control}
                 name="ownershipType"
